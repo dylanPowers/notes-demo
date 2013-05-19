93ac8d754dee721a23e499faad18c71c390bd94d
Dylan Powers <dylan.powers@email.wsu.edu>
2013-05-18 22:34:28 -0700

    Adding in the Note class and a class for storage of a Note We'll get to filling in the details later.

---
+++ NotesDemo/src/main/java/me/kickassdemos/notesdemo/note/Note.java
@@ -0,0 +1,15 @@
+package me.kickassdemos.notesdemo.note;
+
+
+
+/**
+ * Created by Dylan on 5/18/13.
+ */
+public class Note {
+    public String title;
+    public String content;
+
+    public Note(String title, String content) {
+
+    }
+}
---
+++ NotesDemo/src/main/java/me/kickassdemos/notesdemo/note/NoteStorage.java
@@ -0,0 +1,24 @@
+package me.kickassdemos.notesdemo.note;
+
+import java.util.Vector;
+
+/**
+ * Created by Dylan on 5/18/13.
+ */
+public class NoteStorage {
+
+    public NoteStorage() {
+
+    }
+
+
+    void save(Note note) {
+
+    }
+
+    Vector<Note> loadAll() {
+
+        // TODO "Actually return something."
+        return null;
+    }
+}
